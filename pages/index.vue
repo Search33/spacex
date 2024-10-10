237f10772f198294cae1c31cb9d6aade31a367e9
Jack <71765687+Search33@users.noreply.github.com>
2024-10-10 16:02:32 +1100

    data

--- pages/index.vue
+++ pages/index.vue
@@ -118,8 +118,8 @@ useHead({
     title: 'Rocket Launches'
 })
 
-   const { data: launches } = await useFetch('http://localhost:3000/launches.json')
-// const { data: launches } = useFetch('https://fdo.rocketlaunch.live/json/launches/next/5')
+//    const { data: launches } = await useFetch('http://localhost:3000/launches.json')
+const { data: launches } = useFetch('https://fdo.rocketlaunch.live/json/launches/next/5')
 
 const { providers } = useProviders()
 const { validTags } = useValidTags(launches)
